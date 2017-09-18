abfe235ff1a9df48376e7186d011c94d75294a82
Carlos L. Barahona <carlos.barahona1995@gmail.com>
2017-09-17 19:04:28 -0500

    Add files via upload

---
+++ Second Portfolio Project/script.js
@@ -0,0 +1,5 @@
+window.sr = ScrollReveal();
+sr.reveal('.introInside', { duration: 2500, mobile: true });
+sr.reveal('.aboutmeInside', { duration: 2500 });
+sr.reveal('.bioInside', { duration: 2500 });
+sr.reveal('.portfolioInside', { duration: 2500 });
